d3ac7f7807e63923114894be48db89c8600524ff
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-27 12:55:57 +0100

    add: global context to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GlobalContext from './contexts/GlobalContext'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import PostsList from "./pages/PostsList"
@@ -8,22 +9,25 @@ import DefaultLayout from './pages/DefaultLayout'
 
 
 function App() {
-
+  const baseUrl = "http://127.0.0.1:3000/"
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/posts'>
-              <Route index element={<PostsList />} />
-              <Route path=':slug' element={<SinglePost />} />
+      <GlobalContext.Provider value={{ baseUrl }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/posts'>
+                <Route index element={<PostsList />} />
+                <Route path=':slug' element={<SinglePost />} />
+              </Route>
+              <Route path='*' element={<NotFound />}></Route>
             </Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
+
     </>
   )
 }
